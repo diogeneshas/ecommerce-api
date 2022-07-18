1c6f0ae31365837b46ad9ebc27233e9d0bdb9ea2
diogeneshas <diogenes.henrique.br@gmail.com>
2022-07-18 07:24:46 -0300

    generating model category and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_124853) do
+ActiveRecord::Schema.define(version: 2022_07_18_100943) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
