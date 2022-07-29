96749c8e0de8474cbb6ca88ac5868614ba22bb49
diogeneshas <diogenes.henrique.br@gmail.com>
2022-07-29 10:01:35 -0300

    feat: aplicando os concerns aos outros models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  include NameSearchable
+  include Paginatable
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
